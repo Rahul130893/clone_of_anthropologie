8a38b04332d591f345118db9e5da3f7190f6cbce
Rahul <rahulrdx1308@gmail.com>
2022-08-27 13:56:07 +0530

    backend done

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,14 @@
 const express = require("express");
-
+const User= require("../models/user.model")
 const router = express.Router();
+router.get("", async (req, res) => {
+  try {
+    const users = await User.find().lean().exec();
+
+    return res.status(200).send({ users : users }); // []
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
 
 module.exports = router;
